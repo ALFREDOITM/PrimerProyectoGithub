e50bc44ae6e181c97ad0a9e25d23f31316461c89
ALFREDOITM <alfredosanchez.as68@gmail.com>
2022-03-09 21:02:55 -0600

    Part 2 calculator

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.calculadora
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonClear.setOnClickListener{
+            Toast.makeText(this, "ejemplo", Toast.LENGTH_LONG).show()
+        }
     }
 }
